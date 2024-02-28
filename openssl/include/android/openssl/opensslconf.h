ee700faa229a31e84161bf5151e46897f78ef0ff
Goodleeches <goodleeches@gmail.com>
2024-02-28 15:53:43 +0900

    Update opensslconf.h

--- openssl/include/android/openssl/opensslconf.h
+++ openssl/include/android/openssl/opensslconf.h
@@ -4,6 +4,8 @@
 #include "opensslconf-arm32.h"
 #elif __i386__
 #include "opensslconf-x86.h"
+#elif defined(__x86_64__) || defined(__x86_64) || defined(__amd64__) || defined(__amd64) || defined(_M_X64) || defined(_M_AMD64)
+#include "opensslconf-x86_64.h"
 #else
 #error "Unsupported architecture!"
 #endif
